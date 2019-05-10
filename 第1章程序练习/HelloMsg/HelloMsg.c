58aac26e51f7c3db8f3d651c93c5991e9e25ef50
SAO <1053001374@qq.com>
2019-05-10 18:54:19 +0800

    ...

--- 第1章程序练习/HelloMsg/HelloMsg.c
+++ 第1章程序练习/HelloMsg/HelloMsg.c
@@ -1,6 +1,7 @@
 #include<Windows.h>
+#include<tchar.h>
 int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPreInstance, LPSTR IpCmdLine, int nShowCmd)
 {
-	MessageBox(NULL, TEXT("Hello,Windows!"), TEXT("HelloMsg"), 0);
+	MessageBox(NULL, TEXT("Hello,Windows!"), TEXT("HelloMsg"), 2);
 	return 0;
 }
